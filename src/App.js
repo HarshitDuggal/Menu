7198f75b6b84b50368a3e1dac081d872fd27e86d
Harshit Duggal <harshitduggal101@gmail.com>
2021-07-21 02:04:37 +0530

    display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+import menu from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuitems, setmenuitems] = useState(items);
+  const [ctegories, setctegories] = useState([]);
+  return <main>
+      <section className ='menu'>
+      <div className='title'> 
+        <h2>Our Menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories/>
+      <Menu item = {menuitems}/>
+    </section>;
+  </main>
+  
 }
 
 export default App;
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({item}) => {
+  return <div className='section-center'>
+    {item.map((menuitem) => {
+      const {id,title,category,price,img,desc} = menuitem
+      return (
+        <article className ='menu-item' key={id}>
+          <img src={img} alt={title} className='photo'/>
+          <div className='item-info'>
+            <header>
+              <h4>{title}</h4>
+              <h4 className='price'>${price}</h4>
+            </header>
+            <p className='item-text'>{desc}</p>
+          </div>
+        </article>
+      )
+    })}
+  </div>;
 };
 
 export default Menu;
